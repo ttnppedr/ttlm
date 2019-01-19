1d887e662e4de7e0dc59953150182e5b02d6223c
ttnppedr <priest0204@gmail.com>
2019-01-19 18:30:00 +0800

    [Feature] index streams api

--- app/Http/Controllers/StreamController.php
+++ app/Http/Controllers/StreamController.php
@@ -29,4 +29,34 @@ class StreamController extends Controller
             'data' => $stream,
         ], 200, [], JSON_UNESCAPED_UNICODE);
     }
+
+    public function index()
+    {
+        $validator = Validator::make($request = request()->all(), [
+            'q' => 'string',
+            'row' => 'integer',
+            'page' => 'integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'result' => 'fail',
+                'message' => $validator->errors()->first(),
+            ], 200, [], JSON_UNESCAPED_UNICODE);
+        }
+
+        $q = $request['q'] ?? '';
+        $row = $request['row'] ?? 20;
+        $page = $request['page'] ?? 1;
+
+        return response()->json([
+            'result' => 'success',
+            'data' => Stream::where('title', 'LIKE', '%' . $q . '%')
+                ->orWhere('url', 'LIKE', '%' . $q . '%')
+                ->orWhere('played_at', 'LIKE', '%' . $q . '%')
+                ->skip($row * ($page - 1))
+                ->take($row)
+                ->get()
+        ], 200, [], JSON_UNESCAPED_UNICODE);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::POST('/streams', 'StreamController@store');
+Route::GET('/streams', 'StreamController@index');
